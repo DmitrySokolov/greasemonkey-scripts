654f97a5bf22de2e38479ef33088f3543747d6a1
Dmitry Sokolov <mr.dmitry.sokolov@gmail.com>
2020-04-03 17:12:45 +0300

    Added 'Lenta.ru.user.js' script.

---
+++ Lenta.ru.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name        Lenta.ru
+// @namespace   habr
+// @include     https://lenta.ru/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+!function(){
+  !function addStyle(cssStr) {
+    var D = document;
+    var elem = D.getElementsByTagName('head')[0] || D.body || D.documentElement;
+    var newStyle = D.createElement('style');
+    newStyle.textContent = cssStr;
+    elem.appendChild(newStyle);
+  }('\
+#root {\
+  width: unset !important;\
+  margin-right: 50px !important;\
+}\
+#up {\
+  left: unset !important;\
+  right: 10px !important;\
+}\
+.g-layout {\
+  width: unset !important;\
+  margin-left: 40px !important;\
+  margin-right: 40px !important;\
+}\
+.b-topic-layout__content {\
+  display: flex !important;\
+}\
+.b-topic-layout__right {\
+  float: unset !important;\
+  order: 2 !important;\
+}\
+.b-topic-layout__left {\
+  float: unset !important;\
+  width: unset !important;\
+  margin-right: 40px !important;\
+}\
+.b-topic__socials._header {\
+  display: none !important;\
+}\
+.b-topic .b-text {\
+  font-size: 20px !important;\
+  line-height: 125% !important;\
+}\
+.b-topic .b-text .b-video-box:last-of-type {\
+  display: none !important;\
+}\
+     ');
+}();
